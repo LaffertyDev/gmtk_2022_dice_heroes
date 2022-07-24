ca590f14a34a2fa033c19372e1a61fc4d301b8df
Nathan Lafferty <nslafferty@gmail.com>
2022-07-24 16:52:03 -0500

    improve endless mode

--- game/board/board.gd
+++ game/board/board.gd
@@ -5,6 +5,7 @@ signal new_zone_entered(new_battle)
 signal final_zone_completed()
 var heroes_won_last_match = false
 var hero_current_position = 0
+var board_round = 0
 
 onready var global_audio = get_node("/root/global_audio")
 
@@ -12,6 +13,7 @@ func _on_battle_finished(did_heroes_win):
 	if did_heroes_win:
 		if hero_current_position == 21:
 			emit_signal("final_zone_completed")
+			board_round += 1
 			hero_current_position = 0 # reset so you can loop
 			heroes_won_last_match = true
 			_move_heroes_to_position()
@@ -69,7 +71,8 @@ func _get_next_battle():
 	var rewards = []
 	var gold_income = 0
 	var enemy_group_health = 1
-	match hero_current_position:
+	var board_index = board_round * 21 + hero_current_position
+	match board_index:
 		0:
 			pass
 		1:
@@ -143,7 +146,7 @@ func _get_next_battle():
 			gold_income = 10
 			enemy_group_health = 30
 		14:
-			enemies.append({"enemy_type": "necromancer"}) # 16
+			enemies.append({"enemy_type": "necromancer"}) # 16 - heal meme
 			enemies.append({"enemy_type": "necromancer"})
 			enemies.append({"enemy_type": "mimic"})
 			enemies.append({"enemy_type": "mimic"})
@@ -176,30 +179,35 @@ func _get_next_battle():
 			enemies.append({"enemy_type": "necromancer"})
 			enemies.append({"enemy_type": "necromancer"})
 			gold_income = 10
-			enemy_group_health = 65
+			enemy_group_health = 90
 		19:
 			enemies.append({"enemy_type": "dragon"}) # 23
 			enemies.append({"enemy_type": "dragon"})
 			enemies.append({"enemy_type": "dragon"})
 			enemies.append({"enemy_type": "mimic"})
 			gold_income = 10
-			enemy_group_health = 75
+			enemy_group_health = 115
 		20:
 			enemies.append({"enemy_type": "dragon"}) # 26
 			enemies.append({"enemy_type": "dragon"})
 			enemies.append({"enemy_type": "dragon"})
 			enemies.append({"enemy_type": "dragon"})
 			gold_income = 10
-			enemy_group_health = 100
+			enemy_group_health = 150
 		21:
 			enemies.append({"enemy_type": "boss"}) # 30
 			enemies.append({"enemy_type": "dragon"}) 
 			enemies.append({"enemy_type": "dragon"})
 			enemies.append({"enemy_type": "dragon"})
-			gold_income = 924678
-			enemy_group_health = 150
+			gold_income = 10000
+			enemy_group_health = 250
 		_:
-			pass
+			enemies.append({"enemy_type": "boss"}) # 30
+			enemies.append({"enemy_type": "dragon"}) 
+			enemies.append({"enemy_type": "dragon"})
+			enemies.append({"enemy_type": "dragon"})
+			gold_income = board_index
+			enemy_group_health = board_index * 11
 
 	var battle = {"enemies": enemies, "rewards": rewards, "gold_income": gold_income, "enemy_group_health": enemy_group_health}
 	return battle
