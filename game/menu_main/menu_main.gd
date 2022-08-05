e93d003f61eb363744c7bbd0bbd83cba46fe5162
Nathan Lafferty <nslafferty@gmail.com>
2022-08-05 13:34:54 -0500

    add basic start screen with very bad graphics

---
+++ game/menu_main/menu_main.gd
@@ -0,0 +1,7 @@
+extends Control
+
+onready var global_audio = get_node("/root/global_audio")
+
+func _on_button_start_game_pressed():
+	global_audio.play_ui()
+	var _ig = get_tree().change_scene("res://game/game.tscn")
